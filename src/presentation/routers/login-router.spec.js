d3e1c4b961a04203470018f74bdcf0ab3aa02a44
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2020-03-14 12:30:28 +0100

    [FEATURE][LOGIN TEST] Si en el test no pasamos un email tiene que dar en el statusCode un 400

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -12,7 +12,9 @@ describe('Login Router', () => {
   test('should return 400 if no email is provided', () => {
     const sut = new LoginRouter()
     const httpRequest = {
-
+      body: {
+        password: 'cualquiercosa'
+      }
     }
     const httpResponse = sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
