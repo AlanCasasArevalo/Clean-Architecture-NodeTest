fd7ae5bddaff77d773788339c63813f8b6cf10ae
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2020-03-14 11:51:12 +0100

    [FEATURE][LOGIN] Desacoplamiento de mongoose, desacoplamiento entre capas, creando partes mas sectorizadas

--- login.js
+++ login.js
@@ -1,29 +1,35 @@
-
 // Sign-router
 const express = require('express')
 const router = express.Router()
+
 class SignUpRouter {
   async route (req, res) {
     const { email, password, repeatPassword } = req.body
-    new SignUpUseCase().signUp(email, password, repeatPassword)
-    res.status(400).json({
-      error: 'email, password and repeat must be exists'
-    })
+    if (email && typeof email !== 'undefined' && password && typeof password !== 'undefined' && repeatPassword && typeof repeatPassword !== 'undefined') {
+      new SignUpUseCase().signUp(email, password, repeatPassword)
+    } else {
+      res.status(400).json({
+        error: 'email, password and repeat must be exists'
+      })
+    }
   }
 }
+
 // Sign-use-cases
-const mongoose = require('mongoose')
-const AccountModel = mongoose.model('Account')
 class SignUpUseCase {
   async signUp (email, password, repeatPassword) {
-    if (email && typeof email !== 'undefined' && password && typeof password !== 'undefined' && repeatPassword && typeof repeatPassword !== 'undefined') {
-      if (password === repeatPassword) {
-        const user = await AccountModel.create({
-          email, password
-        })
-        return user
-      }
+    if (password === repeatPassword) {
+      new AddAccountRepository().addNewUser(email, password, repeatPassword)
     }
+  }
+}
+
+const mongoose = require('mongoose')
+const AccountModel = mongoose.model('Account')
+class AddAccountRepository {
+  async addNewUser (email, password, repeatPassword) {
+    const user = await AccountModel.create({ email, password })
+    return user
   }
 }
 
