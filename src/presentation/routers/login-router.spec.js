11912c64bc234a657d69c73c9d295c421d9a3581
Alan Casas Arevalo <acasasar@everis.com>
2020-03-16 19:12:23 +0100

    [FEATURE][LOGIN TEST] Se refactoriza para poder tener unos metodos que creen un caso de autenticacion ko

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -4,6 +4,16 @@ const UnauthorizedError = require('../helpers/unauthorized-error')
 const ServerError = require('../helpers/server-error')
 
 const makeSut = () => {
+  const authUseCaseSpy = makeAuthUseCase()
+  authUseCaseSpy.accessToken = 'valid_token'
+  const sut = new LoginRouter(authUseCaseSpy)
+  return {
+    authUseCaseSpy,
+    sut
+  }
+}
+
+const makeAuthUseCase = () => {
   class AuthUseCaseSpy {
     auth (email, password) {
       this.email = email
@@ -11,13 +21,15 @@ const makeSut = () => {
       return this.accessToken
     }
   }
-  const authUseCaseSpy = new AuthUseCaseSpy()
-  authUseCaseSpy.accessToken = 'valid_token'
-  const sut = new LoginRouter(authUseCaseSpy)
-  return {
-    authUseCaseSpy,
-    sut
+  return new AuthUseCaseSpy()
+}
+const makeAuthUseCaseWithError = () => {
+  class AuthUseCaseSpy {
+    auth () {
+      throw new Error()
+    }
   }
+  return new AuthUseCaseSpy()
 }
 
 describe('Login Router', () => {
@@ -105,6 +117,20 @@ describe('Login Router', () => {
     class AuthUseCaseSpyWithoutAuth {}
     const authWithoutAuthSpy = new AuthUseCaseSpyWithoutAuth()
     const sut = new LoginRouter(authWithoutAuthSpy)
+    const httpRequest = {
+      body: {
+        email: 'any_email@cualquiercosa.com',
+        password: 'any_password'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(500)
+    expect(httpResponse.body).toEqual(new ServerError())
+  })
+
+  test('Should return 500 if no AuthUseCase throws', () => {
+    const authUseCaseSpy = makeAuthUseCaseWithError()
+    const sut = new LoginRouter(authUseCaseSpy)
     const httpRequest = {
       body: {
         email: 'any_email@cualquiercosa.com',
