469677d4ecdc6bc1a1b5b1fdadfe5ba30fb84efd
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2020-03-22 09:16:28 +0100

    [FEATURE][EMAIL VALIDATOR] Se hace lo minimo para que pase el test

--- src/presentation/utils/email-validator.spec.js
+++ src/presentation/utils/email-validator.spec.js
@@ -1,3 +1,8 @@
+class EmailValidator {
+  isValid (email) {
+    return true
+  }
+}
 describe('Email Validator', () => {
   test('Should return true if validator return true', () => {
     const sut = new EmailValidator()
