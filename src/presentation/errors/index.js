e57fd50102e6c1ff3bf99b5cbc314c7d7b507bfd
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2020-03-22 09:09:47 +0100

    [FEATURE][INDEX ERROR] Nuevo index fue creado para poder hacer directamente las importaciones de los errores sin tener que hacer tantos require.

---
+++ src/presentation/errors/index.js
@@ -0,0 +1,13 @@
+const MissingParamError = require('./missing-param-error')
+const ServerError = require('./server-error')
+const UnauthorizedError = require('./unauthorized-error')
+const InvalidParamError = require('./invalid-param-error')
+
+module.exports = {
+  MissingParamError,
+  ServerError,
+  UnauthorizedError,
+  InvalidParamError
+}
+
+
