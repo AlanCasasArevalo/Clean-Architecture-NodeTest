b2bde869b7cd1d48625157888b5c284ca9623619
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2020-03-22 09:18:07 +0100

    [FEATURE][EMAIL VALIDATOR] Se crea un nuevo test para que si el email proporcionado no es correcto nos devuelva un false.

--- src/presentation/utils/email-validator.spec.js
+++ src/presentation/utils/email-validator.spec.js
@@ -9,4 +9,10 @@ describe('Email Validator', () => {
     const isEmailValid = sut.isValid('valid_email@mail.com')
     expect(isEmailValid).toBe(true)
   })
+
+  test('Should return false if validator return false', () => {
+    const sut = new EmailValidator()
+    const isEmailValid = sut.isValid('invalid_email@mail.com')
+    expect(isEmailValid).toBe(false)
+  })
 })
