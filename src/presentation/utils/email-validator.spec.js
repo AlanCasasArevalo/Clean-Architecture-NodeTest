322e01da5ab0f4917f3b1b924fa7a7ad5272dc47
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2020-03-22 09:15:07 +0100

    [FEATURE][EMAIL VALIDATOR] Se ha creado el test que va a comprobar si el email proporcionado es valido.

---
+++ src/presentation/utils/email-validator.spec.js
@@ -0,0 +1,7 @@
+describe('Email Validator', () => {
+  test('Should return true if validator return true', () => {
+    const sut = new EmailValidator()
+    const isEmailValid = sut.isValid('valid_email@mail.com')
+    expect(isEmailValid).toBe(true)
+  })
+})
